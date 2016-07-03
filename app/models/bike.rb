aa64706e10b33280ab32d80676fe9e67359d6537
dinko azema <rocket4ce@MBP-de-dinko.home>
2016-07-02 19:17:27 -0500

    validations models bike

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,3 +1,5 @@
 class Bike < ActiveRecord::Base
   validates :color,:brand, :description,:ring, :photo , presence: true
+  validates :description, length: {minimum: 140}
+  validates :ring,  numericality: { greater_than: 0 }
 end
